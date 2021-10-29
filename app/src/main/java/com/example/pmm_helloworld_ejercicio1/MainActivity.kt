93d1ac468d494b18339909c6b010a1fa6d4ef643
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-29 10:56:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pmm_helloworld_ejercicio1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pmm_helloworld_ejercicio1
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+}
